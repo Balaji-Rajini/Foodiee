61447645c74e114e211be92344f9f50c2a59dc8c
Balaji-Rajini <balajitheprogrammer@gmail.com>
2025-08-24 21:46:01 +0530

    eroor fixed here

--- src/Components/Dishes.jsx
+++ src/Components/Dishes.jsx
@@ -1,7 +1,6 @@
 import { useState ,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 import { BsCart, BsCartFill } from "react-icons/bs";
-import Search from "./Search";
 
 
 
